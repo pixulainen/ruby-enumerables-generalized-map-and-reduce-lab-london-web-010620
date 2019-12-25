64a477c4c0e7da721a89b30cc6f874b59dce47b1
George Apetrei <george.apetrei@gmail.com>
2019-12-25 13:13:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,7 +12,19 @@ end
 
 #my own reduce
 def reduce(array, sv = nil)
+ if sv 
+   sum = sv
+   i = 0 
+ else 
+   sum= array[0]
+   i = 1 
+ end
  
+ whilve  i < array.size 
+sum =  yield(sum,array[i])
+ i+=1 
+ end
+ sum
 end
 
 
