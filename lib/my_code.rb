a8c8cb5df5059665b4ff45360e3935f4fec942d2
George Apetrei <george.apetrei@gmail.com>
2019-12-25 13:16:18 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,40 @@
 # Your Code Here
+#my own map
+def map(array)
+  new=[]
+  counter = 0 
+  while counter < array.size 
+  new.push(yield(array[counter]))
+  counter+=1
+  end
+  new
+end
+
+#my own reduce
+def reduce(array, sv = nil)
+ if sv 
+   sum = sv
+   i = 0 
+ else 
+   sum= array[0]
+   i = 1 
+ end
+ 
+ while  i < array.size 
+  sum =  yield(sum,array[i])
+ i+=1 
+ end
+ sum
+end
+
+
+
+
+
+
+
+
+
+
+
+
